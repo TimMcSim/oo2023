a2af3befdb157a773b5cc45c2f0e2d09d51f5b46
TimMcSim <60642739+TimMcSim@users.noreply.github.com>
2023-02-13 14:26:53 +0200

    Update kt01.ts

--- 01_funktsioon/kt01.ts
+++ 01_funktsioon/kt01.ts
@@ -3,7 +3,7 @@ let klaviatuur=require("readline-sync")
 let word:string=klaviatuur.question("Sisesta oma sõna:");
 
 function charinword(inputWord: string) {
-	let inputWordLenght:string = inputWord.length;
+	let inputWordLenght:number = inputWord.length;
 	let answer:string = "Sinu sõna pikus on: "+ inputWordLenght+" tähte"
 	return answer
 }
